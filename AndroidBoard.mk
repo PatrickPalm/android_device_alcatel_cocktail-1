e3566e20fb34a9263f081abadae0e37a56a0dd7b
PatrickPalm <patrick.palm.almhult@gmail.com>
2013-03-26 22:17:29 +0100

    Adapted tree from Jellybean

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -2,5 +2,3 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
-
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/device_cocktail.mk
+    $(LOCAL_DIR)/full_cocktail.mk
